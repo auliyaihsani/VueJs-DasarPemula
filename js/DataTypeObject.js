38a10498c86d1d1b1edee37bd09b567b46aab0b0
auliyaihsani <auliyaihsani@gmail.com>
2019-05-25 21:30:05 +0700

    initial commit

---
+++ js/DataTypeObject.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+    el: '#app',
+    data:{
+     vehicles: [
+            {type: 'Car', brand: 'Toyota', color: 'blue'},
+            {type: 'Motorcycle', brand: 'yamaha', color: 'red'},
+            {type: 'Bike', brand: 'bmx', color: 'black'},
+        ],
+    
+    object: {
+        firstName: 'john',
+        lastName:'wick',
+        age:20
+        }
+    }    
+})
